b1ffa0935c30b165322634a600251a981c633d4b
DamirGDR <71948762+DamirGDR@users.noreply.github.com>
2025-12-09 13:18:38 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -4047,7 +4047,7 @@ def main():
                                                                END, tbh.id ORDER BY tbh."timestamp" DESC) AS rn
                     FROM damir.t_bike_history tbh
                     WHERE tbh.error_status IN (0,7)
-                        AND tbh."timestamp" >= CURRENT_DATE - INTERVAL '2 hour'
+                        AND tbh."timestamp" >= CURRENT_DATE + INTERVAL '1 hour'
                         AND tbh."timestamp" <= NOW() + INTERVAL '2 hour'
                         ) AS res_tab
                 CROSS JOIN damir.t_area ta 
