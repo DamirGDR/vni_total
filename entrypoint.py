11da5a1e71da95165544fb59aaac711ececa140f
DamirGDR <rimad91@yandex.ru>
2026-02-09 11:15:06 +0300

    Аргос склад АКБ = Волос своб АКБ

--- entrypoint.py
+++ entrypoint.py
@@ -1242,13 +1242,16 @@ def main():
                  'spisannye',
                  'donory',
                  'samokaty_v_puti']]
-        df.loc[(df['city_id'] == 12) & (df['name'] == 'Argos Orestiko_freego v3pro'), 'svobodnyh_akb'] = 0
-        df.loc[(df['city_id'] == 12) & (df['name'] == 'Argos Orestiko_freego v.4.6.'), 'svobodnyh_akb'] = 0
+        df.loc[(df['city_id'] == 12) & (df['name'] == 'Argos Orestiko_freego v3pro'), 'svobodnyh_akb'] = int(
+        df3.loc[df3['city_name'] == 'Аргос (склад Волоса)', 'Batteries numbers V3 PRO'].iloc[0])
+        df.loc[(df['city_id'] == 12) & (df['name'] == 'Argos Orestiko_freego v.4.6.'), 'svobodnyh_akb'] = int(
+        df3.loc[df3['city_name'] == 'Аргос (склад Волоса)', 'Batteries V4.6'].iloc[0])
         df.loc[(df['city_id'] == 11) & (df['name'] == 'Kastoria_freego v3pro'), 'svobodnyh_akb'] = int(
             df3.loc[df3['city_name'] == 'Кастория (склад)', 'Batteries numbers V3 PRO'].iloc[0])
         df.loc[(df['city_id'] == 11) & (df['name'] == 'Kastoria_freego v.4.6.'), 'svobodnyh_akb'] = int(
             df3.loc[df3['city_name'] == 'Кастория (склад)', 'Batteries V4.6/V4.7'].iloc[0])
-        df.loc[(df['city_id'] == 11) & (df['name'] == 'Kastoria_freego v.4.7.'), 'svobodnyh_akb'] = 0
+        df.loc[(df['city_id'] == 11) & (df['name'] == 'Kastoria_freego v.4.7.'), 'svobodnyh_akb'] = int(
+        df3.loc[df3['city_name'] == 'Кастория (склад)', 'Batteries V4.6/V4.7'].iloc[0])
         df = df.replace('Argos Orestiko_freego v3pro', 'Малый склад_v3pro') \
             .replace('Argos Orestiko_freego v.4.6.', 'Малый склад_v.4.6') \
             .replace('Argos Orestiko_freego v.4.7.', 'Малый склад_v.4.7') \
